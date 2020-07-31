7a2940f5732deef81401ce6c54189ffee43c1325
Edward Distel <edward.distel@gmail.com>
2020-07-31 18:35:51 -0400

    Move app files to src

---
+++ src/getLeaguePrefix.js
@@ -0,0 +1,21 @@
+const yahoo = require('./yahooFantasyBaseball');
+
+const getData = async() => {
+    try {
+        // Read credentials file or get new authorization token
+        await yahoo.yfbb.readCredentials();
+        
+        // If crededentials exist
+        if (yahoo.yfbb.CREDENTIALS) {
+        
+        // Output the league prefix
+        console.log(await yahoo.yfbb.getLeaguePrefix());
+
+        }
+    } catch (err) {
+        console.error(`Error in getData(): ${err}`);
+    }
+
+};
+
+getData();
